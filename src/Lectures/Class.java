4744d56080bd2c462316c7648b1b123742216447
EgorZak <egorzakruzhnui@gmail.com>
2022-04-21 10:28:32 +0300

    class

---
+++ src/Lectures/Class.java
@@ -0,0 +1,14 @@
+package Lectures;
+
+import java.util.Scanner;
+
+public class Class {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
